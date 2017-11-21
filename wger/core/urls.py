0f9aa089f9461578b6914c6564095dec458f73f0
kimotho-njoki <graycenjoki@gmail.com>
2017-11-21 16:29:01 +0300

    [Feature] Add fitbit url path

--- wger/core/urls.py
+++ wger/core/urls.py
@@ -97,6 +97,9 @@ patterns_user = [
     url(r'^list',
         user.UserListView.as_view(),
         name='list'),
+    url(r'^fetch_fitbit_data',
+        user.get_fitbitData,
+        name='fitbitData'),
 
     # Password reset is implemented by Django, no need to cook our own soup here
     # (besides the templates)
