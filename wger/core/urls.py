08a0430ad3925883a37faa3435f51a9988c82bf0
Muthomi Mate <muthomimate@gmail.com>
2017-11-22 15:26:48 +0300

    [Feature]add social logins

--- wger/core/urls.py
+++ wger/core/urls.py
@@ -54,6 +54,7 @@ patterns_language = [
 
 # sub patterns for user
 patterns_user = [
+
     url(r'^login$',
         user.login,
         name='login'),
@@ -190,6 +191,8 @@ urlpatterns = [
         misc.dashboard,
         name='dashboard'),
 
+    
+
     # Others
     url(r'^about$',
         TemplateView.as_view(template_name="misc/about.html"),
@@ -202,7 +205,8 @@ urlpatterns = [
         name='feedback'),
 
     url(r'^language/', include(patterns_language, namespace="language")),
-    url(r'^user/', include(patterns_user, namespace="user")),
+    url(r'^user/', include(patterns_user, namespace="user")), 
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^license/', include(patterns_license, namespace="license")),
     url(r'^repetition-unit/', include(patterns_repetition_units, namespace="repetition-unit")),
     url(r'^weight-unit/', include(patterns_weight_units, namespace="weight-unit")),
