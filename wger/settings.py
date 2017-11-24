18ff58ad66bf7a30ed2eba82fc4b2b7b49fde8d2
Vincent Asante <vincenthokie@gmail.com>
2017-11-24 14:40:50 +0300

    [Feature #153114424] update statics config in settings

--- wger/settings.py
+++ wger/settings.py
@@ -80,8 +80,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/wger/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
