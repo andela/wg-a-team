452485a08c5159f13deee0531641f2ef7ccae52f
Vincent Asante <vincenthokie@gmail.com>
2017-11-24 09:01:49 +0300

    roll-back, update settings

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -378,9 +378,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
