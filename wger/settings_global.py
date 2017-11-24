75e567374e0333270418be2c3a2a07b5c6e00f0b
Vincent Asante <vincenthokie@gmail.com>
2017-11-24 08:58:54 +0300

    roll-back, update settings

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -374,7 +374,7 @@ WGER_SETTINGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
