806c1d974d64134834d66add132ea8690d75bbc2
kisakyegordon <gordon56692@gmail.com>
2017-12-04 15:52:46 +0300

    Add init modifications

--- settings.py
+++ settings.py
@@ -2,44 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from wger.settings_global import *
-import os
 
 # Use 'DEBUG = True' to get more details for server errors
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = True
+
 
 ADMINS = (
     ('Your name', 'your_email@example.com'),
 )
 MANAGERS = ADMINS
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': '/app/database.sqlite',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("5432"),
-        'TEST': {'CHARSET': 'UTF8'}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/Users/elgordo/.local/share/wger/database.sqlite',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'mkj!4--wke3jp#)boeb#9a($e(7j9xle&j^w_-%63(n8u%=8e8'
+SECRET_KEY = '3l(r1%+*2xyjnjjm#-osxcn6a5is6id6$5^o9if0*l$&i5&l1w'
 
 # Your reCaptcha keys
 RECAPTCHA_PUBLIC_KEY = ''
@@ -52,15 +39,17 @@ NOCAPTCHA = True
 SITE_URL = 'http://localhost:8000'
 
 # Path to uploaded files
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/Users/vincentasantehokie/.local/share/wger/media'
+# Absolute filesystem path to the directory that will hold
+# user-uploaded files.
+MEDIA_ROOT = '/Users/elgordo/.local/share/wger/media'
 MEDIA_URL = '/media/'
 
-# Allow all hosts to access the application. Change if used in production.
+# Allow all hosts to access the application. Change if used in
+# production.
 ALLOWED_HOSTS = '*'
 
 # This might be a good idea if you setup memcached
-# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 # Configure a real backend in production
 if DEBUG:
@@ -70,13 +59,4 @@ if DEBUG:
 WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
 
 # Your twitter handle, if you have one for this instance.
-# WGER_SETTINGS['TWITTER'] = ''
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/wger/static/'
+#WGER_SETTINGS['TWITTER'] = ''
