c7074122b64dac88db5b03eab13f87bf061eeffa
Vincent Asante <vincenthokie@gmail.com>
2017-12-05 06:54:25 +0300

    [Feature #152919324] correct path of settings file in the manage.py file

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys, os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 from django.core.management import execute_from_command_line
 
@@ -14,8 +14,9 @@ if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
+        print(BASE_DIR)
         setup_django_environment(
-            os.path.join(BASE_DIR, "wg-a-team/wger/settings.py"))
+            os.path.join(BASE_DIR, "wger/settings.py"))
 
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
