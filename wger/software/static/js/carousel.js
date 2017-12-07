0db1f61f914872a0d436779dcca956fec3f41b20
CK Wagaba <ckwagaba@gmail.com>
2017-12-07 14:59:06 +0300

    add carousel scipt

---
+++ wger/software/static/js/carousel.js
@@ -0,0 +1,26 @@
+/* Script to animate software features. Needs improvement. */
+$( document ).ready( function() {
+  // Start script when document is ready
+	animateCarouselItems();
+});
+
+function animateCarouselItems() {
+
+	var current_item = 0; // start at the first item
+	var container_width = $('#carousel_container').width(); // responsive width: viewport width
+	var item_list = $('#carousel_container ul'); // item container
+
+	setInterval( function() {
+
+		// console.log(item_list.children());
+
+		// iterate over list of items, sliding the current to the left, by the viewport width
+		// hiding it and revealing the next at the end of the animation
+		$(item_list.children()[current_item]).animate({marginLeft: - container_width}, 1000);
+		// then add current item to the end of the list for purposes of looping the animation
+		item_list.append(item_list.children()[current_item].outerHTML);
+		current_item ++;
+
+
+	}, 7000 );
+}
