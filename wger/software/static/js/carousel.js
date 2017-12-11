f2248962a47a838b581037f68598248d68b61673
CK Wagaba <ckwagaba@gmail.com>
2017-12-11 18:11:15 +0300

    fix linting errors

--- wger/software/static/js/carousel.js
+++ wger/software/static/js/carousel.js
@@ -2,19 +2,16 @@
 function animateCarouselItems() {
   var currentItem = 0; // start at the first item
   var containerWidth = $('#carousel_container').width(); // responsive width: viewport width
-	var itemList = $('#carousel_container ul'); // item container
+  var itemList = $('#carousel_container ul'); // item container
 
-	setInterval(function () {
+  setInterval(function () {
 		// console.log(itemList.children());
-
 		// iterate over list of items, sliding the current to the left, by the viewport width
 		// hiding it and revealing the next at the end of the animation
-		$(itemList.children()[currentItem]).animate({ marginLeft: - containerWidth }, 1000);
+    $(itemList.children()[currentItem]).animate({ marginLeft:-containerWidth }, 1000);
 		// then add current item to the end of the list for purposes of looping the animation
-		itemList.append(itemList.children()[currentItem].outerHTML);
-		currentItem ++;
-
-
+    itemList.append(itemList.children()[currentItem].outerHTML);
+    currentItem++;
 	}, 3000);
 }
 
