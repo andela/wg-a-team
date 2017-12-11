832429dfd6c185a6495b9c696188a13240c00524
CK Wagaba <ckwagaba@gmail.com>
2017-12-11 12:52:27 +0300

    correct js linting

--- wger/software/static/js/carousel.js
+++ wger/software/static/js/carousel.js
@@ -1,26 +1,24 @@
 /* Script to animate software features. Needs improvement. */
-$( document ).ready( function() {
-  // Start script when document is ready
-	animateCarouselItems();
-});
-
 function animateCarouselItems() {
+  var currentItem = 0; // start at the first item
+  var containerWidth = $('#carousel_container').width(); // responsive width: viewport width
+	var itemList = $('#carousel_container ul'); // item container
 
-	var current_item = 0; // start at the first item
-	var container_width = $('#carousel_container').width(); // responsive width: viewport width
-	var item_list = $('#carousel_container ul'); // item container
-
-	setInterval( function() {
-
-		// console.log(item_list.children());
+	setInterval(function () {
+		// console.log(itemList.children());
 
 		// iterate over list of items, sliding the current to the left, by the viewport width
 		// hiding it and revealing the next at the end of the animation
-		$(item_list.children()[current_item]).animate({marginLeft: - container_width}, 1000);
+		$(itemList.children()[currentItem]).animate({ marginLeft: - containerWidth }, 1000);
 		// then add current item to the end of the list for purposes of looping the animation
-		item_list.append(item_list.children()[current_item].outerHTML);
-		current_item ++;
+		itemList.append(itemList.children()[currentItem].outerHTML);
+		currentItem ++;
 
 
-	}, 3000 );
+	}, 3000);
 }
+
+$(document).ready(function () {
+  // Start script when document is ready
+  animateCarouselItems();
+});
