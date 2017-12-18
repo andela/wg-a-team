69e9ad4622bdacabe4ba1ee34934f4d599072462
Vincent Asante <vincenthokie@gmail.com>
2017-12-18 19:32:34 +0300

    [Feature #153468949] get users belonging to a particualar gym using the gymuserconfig and gymadminconfig models instead of the userprofile as only a single user can be linked to a single gym using the userprofile model

--- wger/gym/managers.py
+++ wger/gym/managers.py
@@ -36,7 +36,7 @@ class GymManager(models.Manager):
         perm_gyms = Permission.objects.get(codename='manage_gyms')
         perm_trainer = Permission.objects.get(codename='gym_trainer')
 
-        users = User.objects.filter(userprofile__gym_id=gym_pk)
+        users = User.objects.filter(gymuserconfig__gym_id=gym_pk)
         return users.exclude(Q(groups__permissions=perm_gym) |
                              Q(groups__permissions=perm_gyms) |
                              Q(groups__permissions=perm_trainer)).distinct()
@@ -49,7 +49,7 @@ class GymManager(models.Manager):
         perm_gyms = Permission.objects.get(codename='manage_gyms')
         perm_trainer = Permission.objects.get(codename='gym_trainer')
 
-        users = User.objects.filter(userprofile__gym_id=gym_pk)
+        users = User.objects.filter(gymadminconfig__gym_id=gym_pk)
         return users.filter(Q(groups__permissions=perm_gym) |
                             Q(groups__permissions=perm_gyms) |
                             Q(groups__permissions=perm_trainer)).distinct()
